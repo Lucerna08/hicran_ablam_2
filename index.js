7fb3296fad3884824327beac2e72faa10f7acd30
Fatmanur Köroğlu <140976432+Lucerna08@users.noreply.github.com>
2024-02-03 13:34:10 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+// function navbar(visible) {
+//     var visible = false;
+    
+//     if(visible) {
+//         document.getElementById("links").style.visibility = "visible";
+//         visible = true;
+//     }else {
+//         document.getElementById("links").style.visibility = "hidden";
+//         visible = false;
+//     }
+// }
+
+// let visible = false;
+
+// function navbar(visible) {
+//     if (visible) {
+//         document.getElementById("links").style.visibility = "visible";
+//         visible = true;
+//     } else {
+//         document.getElementById("links").style.visibility = "hidden";
+//         visible = false;
+//     }
+// }
